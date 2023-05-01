552d741db382a2c20bd2bd9f6b41dd495385a9a6
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2023-05-01 21:57:54 +0300

    Delete backend/dist directory

--- backend/dist/notes/dto/create-note.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { Types } from "mongoose";
-export declare class CreateNoteDto {
-    title: string;
-    body: string;
-    user_id: Types.ObjectId;
-}
--- backend/dist/notes/notes.interface.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-import { Types } from 'mongoose';
-export interface INote extends Document {
-    title: string;
-    body: string;
-    user_id: Types.ObjectId;
-}
