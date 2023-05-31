a68cc2c82c6fe1dc382e099ef01fa3572676f62c
haileadugna <hordofahaile7@gmail.com>
2023-06-01 00:40:31 +0300

    appointment, core, assets commit

--- frontend/lib/core/constants/assets.dart
+++ frontend/lib/core/constants/assets.dart
@@ -17,9 +17,17 @@ class Assets {
   /// assets/icons/search_icon.svg
   static const String assetsIconsSearchIcon = "assets/icons/search_icon.svg";
 
+  /// Assets for assetsImagesAppointmentImage
+  /// assets/images/appointmentImage.png
+  static const String assetsImagesAppointmentImage = "assets/images/appointmentImage.png";
+
   /// Assets for assetsImagesArrow
   /// assets/images/arrow.png
   static const String assetsImagesArrow = "assets/images/arrow.png";
+
+  /// Assets for assetsImagesCalendarImage
+  /// assets/images/CalendarImage.png
+  static const String assetsImagesCalendarImage = "assets/images/CalendarImage.png";
 
   /// Assets for assetsImagesCommunity
   /// assets/images/community.png
@@ -41,6 +49,10 @@ class Assets {
   /// assets/images/Ellipse8.png
   static const String assetsImagesEllipse8 = "assets/images/Ellipse8.png";
 
+  /// Assets for assetsImagesFancyBack
+  /// assets/images/fancyBack.png
+  static const String assetsImagesFancyBack = "assets/images/fancyBack.png";
+
   /// Assets for assetsImagesFavorite
   /// assets/images/Favorite.png
   static const String assetsImagesFavorite = "assets/images/Favorite.png";
@@ -48,6 +60,10 @@ class Assets {
   /// Assets for assetsImagesFitnessBracelet
   /// assets/images/fitness_bracelet.png
   static const String assetsImagesFitnessBracelet = "assets/images/fitness_bracelet.png";
+
+  /// Assets for assetsImagesHalfBackgound
+  /// assets/images/HalfBackgound.png
+  static const String assetsImagesHalfBackgound = "assets/images/HalfBackgound.png";
 
   /// Assets for assetsImagesHappySmileyWithFlowers
   /// assets/images/happy_smiley_with_flowers.png
@@ -60,6 +76,10 @@ class Assets {
   /// Assets for assetsImagesHome
   /// assets/images/home.png
   static const String assetsImagesHome = "assets/images/home.png";
+
+  /// Assets for assetsImagesJoyfulLogo
+  /// assets/images/joyfulLogo.png
+  static const String assetsImagesJoyfulLogo = "assets/images/joyfulLogo.png";
 
   /// Assets for assetsImagesLove
   /// assets/images/Love.png
@@ -76,6 +96,14 @@ class Assets {
   /// Assets for assetsImagesSearch
   /// assets/images/Search.png
   static const String assetsImagesSearch = "assets/images/Search.png";
+
+  /// Assets for assetsImagesSymptomsImage
+  /// assets/images/symptomsImage.png
+  static const String assetsImagesSymptomsImage = "assets/images/symptomsImage.png";
+
+  /// Assets for assetsImagesTipsImage
+  /// assets/images/tipsImage.png
+  static const String assetsImagesTipsImage = "assets/images/tipsImage.png";
 
   /// Assets for assetsImagesWoman
   /// assets/images/woman.png
